97a4e804f5cae19aac609b5e9099a13f040a6015
Krijn Doekemeijer <krijn.doekemeijer@gmail.com>
2024-02-04 21:12:31 +0100

    update run util

--- run-util.sh
+++ run-util.sh
@@ -5,8 +5,7 @@ setup_zenfs() {
     sudo rm -rf /tmp/zenfs
     echo mq-deadline | sudo tee /sys/block/$2/queue/scheduler > /dev/null
     sudo nvme zns reset-zone -a /dev/$2
-    sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/user/src/spdk/dpdk/build/lib/ \
-        ${rocksd_dir}/plugin/zenfs/util/zenfs mkfs \
+    sudo ${rocksd_dir}/plugin/zenfs/util/zenfs mkfs \
             --zbd=$2 \
             --aux_path=/tmp/zenfs \
             --force
